2870d827e279c4f6d1a70754061e94ea9ca2ab08
Carlos Agustin Guanipa Alvarez <agustin_guanipa@hotmail.com>
2019-06-19 11:07:47 -0400

    Arreglos

--- paginas/usuario_availability.php
+++ paginas/usuario_availability.php
@@ -2,11 +2,11 @@
 if(isset($_POST['usuar_clie']))
 {
     // include Database connection file 
-    include("db_connection.php");
+    include("conexion_bd.php");
 
     $usuar_clie = mysqli_real_escape_string($con, $_POST['usuar_clie']);
 
-    $query = "SELECT usuar_clie FROM  WHERE usuar_clie = '$usuar_clie'";
+    $query = "SELECT usuar_clie FROM tabma_clie WHERE usuar_clie = '$usuar_clie'";
     if(!$result = mysqli_query($con, $query))
     {
         exit(mysqli_error($con));
